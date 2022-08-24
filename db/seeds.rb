a58c4f0aac482ea782ec3335fb712aea1a1079f0
Harry Cundall <harry@cundalls.com>
2022-08-24 16:39:14 +0100

    Skeleton there, still need to incorporate functions and make links

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.create([ name: "Golden dragon", address: "2 Pete's Yard", phone_number: "07183674828", category: "chinese" ])
+Restaurant.create([ name: "Pizza Paradise", address: "3 Chambers street", phone_number: "07912046552", category: "italian" ])
+Restaurant.create([ name: "WaffleHaus", address: "99a Portland Avenue", phone_number: "07987654321", category: "belgian" ])
+Restaurant.create([ name: "Garden of Sushi", address: "12 Tokyo Road", phone_number: "012938572", category: "japanese" ])
+Restaurant.create([ name: "Les chateaux du mange", address: "88 Mountbatten Street", phone_number: "07829385739", category: "french" ])
